024fe4c427587b698c0fb9efe1eb8eac9af263ec
Taobo Hu <thuac@connect.ust.hk>
2025-10-03 15:11:20 +0200

    add new feature

--- src/pyXenium/analysis/__init__.py
+++ src/pyXenium/analysis/__init__.py
@@ -1,8 +1,11 @@
 from .protein_gene_correlation import protein_gene_correlation
 from .rna_protein_joint import ProteinModelResult, rna_protein_cluster_analysis
+from .scoring import write_model_scores
+from .plotting import *
 
 __all__ = [
     "protein_gene_correlation",
     "rna_protein_cluster_analysis",
     "ProteinModelResult",
+
 ]
